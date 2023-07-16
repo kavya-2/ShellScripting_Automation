bbfd79b3d814f310246bfefcc02ffd4a84468643
root <root@ip-172-31-11-178.us-west-1.compute.internal>
2023-07-16 13:04:39 +0000

    fix: Update jira token to edit the labels in issues

--- automation2.sh
+++ automation2.sh
@@ -2,5 +2,5 @@
 version="1.2.3.4"
 for line in $(cat jiraid.txt)
 do
-curl -X PUT -u " kavya.sivapal@gmail.com:ATATT3xFfGF0gE7B-yh8c3N7J9avNqs2SzR897GMaRheM5-95gU8TO2uUM5ZfmPwJxcsd9ktgyU8TSrjzIHcnwDmbDXUYrh7YOZ1jmWIpRtqSef7_961b9LxUkQ6n2xcXQyO30tzE44BPABSQye5MaXDhsfOST1WM7yJw4Xv8wMlM-y5uQhSukY=39A5EA2A" --data '{"update":{"labels":[{"add":"DEMO_NEW_BATCH3"}]}}' -H "Content-Type: application/json" https://kavya-99.atlassian.net/rest/api/3/issue/$line
+curl -X PUT -u "kavya.sivapal@gmail.com:ATATT3xFfGF0gE7B-yh8c3N7J9avNqs2SzR897GMaRheM5-95gU8TO2uUM5ZfmPwJxcsd9ktgyU8TSrjzIHcnwDmbDXUYrh7YOZ1jmWIpRtqSef7_961b9LxUkQ6n2xcXQyO30tzE44BPABSQye5MaXDhsfOST1WM7yJw4Xv8wMlM-y5uQhSukY=39A5EA2A" --data '{"update":{"labels":[{"add":"DEMO_NEW_BATCH3"}]}}' -H "Content-Type: application/json" https://kavya-99.atlassian.net/rest/api/3/issue/$line
 done 
