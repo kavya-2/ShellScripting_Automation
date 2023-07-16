1bc3a274835090a2a57ed1a6caf1dcb2c13e48d0
root <root@ip-172-31-11-178.us-west-1.compute.internal>
2023-07-16 14:17:10 +0000

    fix: Update missing quotes

--- automation3.sh
+++ automation3.sh
@@ -2,19 +2,19 @@
 array=(helloservice hiservice nameservice managerservice teamservice)
 for line in "${array[@]}"
 do
-    COUNT=`ps -ef | grep helloservice  | grep -v grep | wc -l`.  RUNNING , 3
+    COUNT='ps -ef | grep helloservice  | grep -v grep | wc -l' #.  RUNNING , 3
     MAX=2
     echo $line
     echo $COUNT
-        if [ $COUNT -gt $MAX ]
+    if [ $COUNT > $MAX ];
     then
-       echo $line
-        PROCS=`ps -ef| grep $line | grep -v grep | awk '{print $2, $11, $12, $13}' | sort -k 4`
-        JAR=`echo "${PROCS}" | awk -F"-Djar_name=| " '{print $5}'`
-        echo $JAR
-        JAR_RUN=`echo $JAR | sed 's/ /,/g'`
-        echo $JAR_RUN
-       cd /apps/nnos/test/scripts
-        ./mail.sh $line $JAR_RUN $COUNT
+      echo $line
+      PROCS=`ps -ef| grep $line | grep -v grep | awk '{print $2, $11, $12, $13}' | sort -k 4`
+      JAR=`echo "${PROCS}" | awk -F"-Djar_name=| " '{print $5}'`
+      echo $JAR
+      JAR_RUN=`echo $JAR | sed 's/ /,/g'`
+      echo $JAR_RUN
+      cd /apps/nnos/test/scripts
+      ./mail.sh $line $JAR_RUN $COUNT
     fi
 done
