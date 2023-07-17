9bd82124652ab978f358ee97f7e7ffef63fa76f2
root <root@ip-172-31-11-178.us-west-1.compute.internal>
2023-07-17 10:39:07 +0000

    fix: Archive/Backup of data with tar command along with hostname and date

--- automation4.sh
+++ automation4.sh
@@ -3,5 +3,5 @@ cd /opt/logs/
 find . -type f -name '*log*' -exec cp '{}' /opt/logs/Log_Backup \; 
 cd /opt/logs/Log_Backup 
 find /opt/logs/Log_Backup -type f -name '*log*' > include-file
-tar -cvf $(hostname)_$(date +%Y-%m-%d).tar.gz -T include-file
+tar -cvf $(hostname)_$(date +%Y-%m-%d).tar.gz --absolute-names -T include-file
 exit
